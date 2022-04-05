24a389f631fda3ed42a85a71fc21f10bfdc9277e
sigseg5 <36568961+sigseg5@users.noreply.github.com>
2022-04-05 21:38:09 +0300

    Init project

---
+++ ua-saver/ua_saverView.h
@@ -0,0 +1,12 @@
+//
+//  ua_saverView.h
+//  ua-saver
+//
+//  Created by Kirill Parhomenko on 05/04/2022.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface ua_saverView : ScreenSaverView
+
+@end
