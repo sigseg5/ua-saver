d33741cd97b3aefd59ee2d86a015c1dada061166
sigseg5 <36568961+sigseg5@users.noreply.github.com>
2022-06-17 17:54:28 +0300

    Fix opacity changing alg

--- ua-saver/saverView.swift
+++ ua-saver/saverView.swift
@@ -4,12 +4,16 @@
 //
 //  Created by sigseg5 on 05/04/2022.
 //
+// Build artefacts at /Users/<user>/Library/Developer/Xcode/DerivedData/ua-saver-*/Build/Products/Debug/ua-saver.saver
 
 import ScreenSaver
 
 class SaverView: ScreenSaverView {
-    private let delay = 0.000008
+    private var count = 0
+    private var reverse = false
     private var opacity = 0.0
+    private let delay = 0.000004
+    private let iterCount = 167_000
     private var screenWidth: CGFloat = 0.0
     private var halfScreenHeight: CGFloat = 0.0
 
@@ -33,8 +37,21 @@ class SaverView: ScreenSaverView {
 
     override func animateOneFrame() {
         super.startAnimation()
-        if opacity <= 1.0 {
+        if reverse == false {
             opacity += delay
+            count += 1
+            if count > iterCount {
+                reverse = true
+                sleep(5)
+            }
+        } else {
+            if count == 0 {
+                reverse = false
+                sleep(2)
+            } else {
+                opacity -= delay
+                count -= 1
+            }
         }
         setNeedsDisplay(bounds)
     }
